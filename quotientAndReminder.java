ae81935b07fdd90f4c9d196b5e4a6741c222a2b9
Sami <sami@Samis-MacBook-Air.local>
2020-05-05 10:09:54 +0530

    Add program for quotientandreminder

---
+++ quotientAndReminder.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class quotientAndReminder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 11;
+		int div = 2;
+		int q = num/div;
+		int rem = num%div;
+		System.out.println(q);
+		System.out.println(rem);
+
+	}
+
+}
