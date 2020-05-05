3f54c3d72e62ec9133c222f9d74eec4d4236fb1b
Sami <sami@Samis-MacBook-Air.local>
2020-05-05 10:24:48 +0530

    Add program for swapping two numbers

---
+++ swapTwoNUmbers.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class swapTwoNUmbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =1;
+		int b = 2;
+		int temp;
+		
+		temp = a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
