9d7037b0dc036bc571c1db07cc3220bff083880c
Sami <sami@Samis-MacBook-Air.local>
2020-05-05 09:05:56 +0530

    add print number

---
+++ programs1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * @author sami
+ *
+ */
+
+
+public class programs1 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number you want to print");
+		int x = in.nextInt();
+		System.out.println("The number you entered is: "+x);	
+
+	}
+
+}
