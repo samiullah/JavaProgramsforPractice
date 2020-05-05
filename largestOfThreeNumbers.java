432f5a5f9eff322a9afd6e5c1fa5991f2d1debb9
Sami <sami@Samis-MacBook-Air.local>
2020-05-05 10:57:21 +0530

    Add program to check largest of 3 numbers'

---
+++ largestOfThreeNumbers.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class largestOfThreeNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 1022;
+		int b =112;
+		int c =1222;
+		
+		if((a>b)&(a>c)) {
+			System.out.println("a is greater");
+		}
+		else if( b > c) {
+			System.out.println("b is greater");
+		}
+		else {
+			System.out.println("c is greatest");
+		}
+
+	}
+
+}
