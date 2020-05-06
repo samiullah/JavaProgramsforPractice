2adc90bad96afc94babd6157e5ae1b37ddb85987
Sami <sami@Samis-MacBook-Air.local>
2020-05-07 05:14:35 +0530

    Add program for character printing a to z

---
+++ displayCharacters.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class displayCharacters {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (char i='A';i<='Z';i++) {
+			
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
