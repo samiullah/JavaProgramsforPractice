ae37b1a5a39307df364385259b9d0fb52182d486
Sami <sami@Samis-MacBook-Air.local>
2020-05-06 17:58:23 +0530

    add program to check sum of first 100 numbers

---
+++ sumOfNaturalNumbers.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class sumOfNaturalNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 0;
+		
+		
+		for (int i= 0; i <=100; i++) {
+			
+			
+			num = num +i;
+			
+			
+		}
+		System.out.println(num);
+
+	}
+
+}
