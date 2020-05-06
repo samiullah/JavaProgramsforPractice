a0fe70da5dbd20e2d002c59ba5fa4346781b70f1
Sami <sami@Samis-MacBook-Air.local>
2020-05-07 04:54:15 +0530

    Add program for multiplication table

---
+++ multiplicationTable.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class multiplicationTable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int num = 2;
+		int mul  = 1;
+		
+		for(int i = 1; i<=10;i++) {
+			
+			mul = num*i;
+			System.out.println(num+"x"+i+"="+mul);
+		
+					
+		}
+
+	}
+
+}
