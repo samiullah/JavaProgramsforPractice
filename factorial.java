643251440c020af73633f4de8c155ad3a332e1f4
Sami <sami@Samis-MacBook-Air.local>
2020-05-07 04:39:53 +0530

    Add program for factorial

---
+++ factorial.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package naveenAutomationPractice;
+
+/**
+ * @author sami
+ *
+ */
+public class factorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//		3! = 3 (3-1)
+		
+		
+		int num = 5;
+		
+		int fact = 1;
+		
+		for(int i =1; i<=num;i++) {
+			fact=fact*i;
+			
+
+}
+		System.out.println(fact);
+	}
+}
